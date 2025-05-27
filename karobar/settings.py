5c07899d81526d75a2cc0344b244f6182cba6a40
bishwas404 <kaflebishwas641@gmail.com>
2025-05-27 16:37:36 +0545

    add env

--- karobar/settings.py
+++ karobar/settings.py
@@ -85,17 +85,17 @@ WSGI_APPLICATION = 'karobar.wsgi.application'
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'my_database',
-        'USER': 'my_user',
-        'PASSWORD': 'my_password',
-        'HOST': 'db',
-        'PORT': '5432',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
